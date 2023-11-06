c9a28970de3c2b7f585ab7c19b56e04593a7f645
osan <osannnn@gmail.com>
2023-11-06 16:37:20 +0100

    added modulus.c and prime.c

---
+++ math/modulus.c
@@ -0,0 +1,44 @@
+#include "math.h"
+#include <stdio.h>
+
+
+int modulus(int a, int b){
+    
+    if (b == 0) {
+        
+        printf("Err.");
+        return -1;
+        
+    }
+    
+    if (a == 0 || b == 1){
+        
+        return 0;
+        
+    }
+    
+    if(b < 0 && a < 0){
+        
+        b = -b;
+        a = -a;
+        
+    }
+    else if (b < 0){
+        
+        b = -b;
+    }
+    else if (a < 0){
+            
+        a = -a;
+        
+    }
+    
+    while(a >= b){
+        
+        a -= b;
+        
+    }
+    
+    return a;
+    
+}
