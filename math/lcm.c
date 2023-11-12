162c7c6046b8631cd53400b56e9c4058f77bbb38
osan <osannnn@gmail.com>
2023-11-12 13:36:42 +0100

    fixed inclusiosns and added abs to formula

--- math/lcm.c
+++ math/lcm.c
@@ -1,6 +1,9 @@
+#include "math.h"
+#include <stdlib.h>
+
 int lcm(int a, int b)
 {
-    int result = divide(multiply(a,b), gcd(a,b));
+    int result = divide(abs(multiply(a,b)), gcd(a,b));
     
     return result;
 }
